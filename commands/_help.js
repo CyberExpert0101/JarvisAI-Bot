07886a4ea247694c077a57d283127c69e4794eaf
Cyber Expert <101377369+CyberExpert0101@users.noreply.github.com>
2023-02-01 08:28:36 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -201,6 +201,15 @@ async(Void, citel, text) => {
         if(cmd.filename) arr.push(`✨FileName: ${cmd.filename}`)
         return await citel.reply(arr.join('\n'));
 
+client.on('message', message => {
+	if(message.body === 'hello') {
+		message.reply('pong');
+	}
+});
+
+
 
 })
+
+
 
